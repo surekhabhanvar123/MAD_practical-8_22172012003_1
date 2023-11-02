fc8fb17ba7573ad6ec2cc89d7ee5dcacd8b5988e
surekhabhanvar123 <surekhabenbhanvar12@gmail.com>
2023-11-02 12:10:51 +0530

    hy

---
+++ app/src/main/java/com/example/mad_practical_8_22172012003/AlarmBroadcastReceiver.kt
@@ -0,0 +1,32 @@
+package com.example.mad_practical_8_22172012003
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class AlarmBroadcastReceiver : BroadcastReceiver() {
+
+    companion object {
+        val ALARMKEY = "KEY"
+        val ALARMSTART = "START"
+        val ALARMSTOP = "STOP"
+    }
+
+    override fun onReceive(context: Context, intent: Intent) {
+        if (intent != null) {
+            val data = intent.getStringExtra(ALARMKEY)
+
+            if (data != null) {
+
+                val intentservice = Intent(context, AlarmService::class.java)
+
+                if (data == ALARMSTART) {
+                    context.startService(intentservice)
+                }
+                else{
+                    context.stopService(intentservice)
+                }
+            }
+        }
+    }
+}
